6b1aa34d2c33f2f4e7874b7fcb4fad6e3b853e18
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-04-01 16:36:12 +0200

    Ajout de classes java

---
+++ Java/src/main/java/edu/m2dl/s10/arge/mapreduce/EntryPoint.java
@@ -0,0 +1,12 @@
+package edu.m2dl.s10.arge.mapreduce;
+
+/**
+ * Created by benoit on 01/04/2016.
+ */
+public class EntryPoint {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Java/src/main/java/edu/m2dl/s10/arge/mapreduce/MapReduceService.java
@@ -0,0 +1,11 @@
+package edu.m2dl.s10.arge.mapreduce;
+
+/**
+ * Created by benoit on 01/04/2016.
+ */
+public class MapReduceService {
+
+
+
+
+}
